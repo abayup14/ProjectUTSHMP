959b87e6bf953285e5439a988e1c851a4861c2c2
stevennchristopher <stchrist03@gmail.com>
2024-04-16 13:26:44 +0700

    add user data

--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class UserserviceService {
 
+  users = [
+    {
+      username: "steven",
+      fullname: "Steven Christopher",
+      url_foto: "",
+      password: "s"
+    },
+    {
+      username: "abayu",
+      fullname: "Andreas Baayu",
+      url_foto: "",
+      password: "a"
+    },
+  ]
   constructor() { }
 }
