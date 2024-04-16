2835962434e5b7dc00e7fc19257c71431f72b847
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-04-16 14:27:24 +0700

    Update Login dan Register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,3 +1,4 @@
+import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { UserserviceService } from '../userservice.service';
 import { Router } from '@angular/router';
@@ -12,26 +13,58 @@ export class RegisterPage implements OnInit {
   new_namaLengkap = ""
   new_url = ""
   new_pass = ""
+  alert_message = ""
 
-  constructor(private userservice: UserserviceService, private router: Router) { }
+  alertButtons = ['OK'];
+
+  constructor(private userservice: UserserviceService, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  register() {
-    this.userservice.addUser(
-      this.new_username,
-      this.new_namaLengkap,
-      this.new_url,
-      this.new_pass
-    )
+  async register() {
+    if (this.userservice.users.find(u => u.username === this.new_username && u.fullname === this.new_namaLengkap)) {
+      this.alert_message = "Gagal Register. Username sudah terdaftar"
+    } else {
+      this.alert_message = "Berhasil Register"
+      this.userservice.addUser(
+        this.new_username,
+        this.new_namaLengkap,
+        this.new_url,
+        this.new_pass
+      )
 
-    this.new_username = ""
-    this.new_namaLengkap = ""
-    this.new_url = ""
-    this.new_pass = ""
+      this.new_username = ""
+      this.new_namaLengkap = ""
+      this.new_url = ""
+      this.new_pass = ""
 
-    this.router.navigate(["/login"])
+      this.router.navigate(["/login"])
+    }
+
+    await this.presentAlert()
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Information',
+      message: this.alert_message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  // konfirmasi = [
+  //   {
+  //     text: "Batal",
+  //     role: "cancel"
+  //   },
+  //   {
+  //     text: "Register",
+  //     role: "confirm",
+  //     handle: () => {
+  //       this.register()
+  //     }
+  //   }
+  // ]
 }
