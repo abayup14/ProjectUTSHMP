831274c57a174e5950bc46274f58bede2c442345
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-04-16 13:26:45 +0700

    Update Register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  new_username = ""
+  new_namaLengkap = ""
+  new_url = ""
+  new_pass = ""
 
   constructor() { }
 
