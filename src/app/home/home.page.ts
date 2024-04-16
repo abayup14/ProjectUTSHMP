46a2999487cdccae29b61e586ac77dcff4514a92
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-04-16 16:22:48 +0700

    Tambah Userbame Tampil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NewsService } from '../news.service';
+import { UserserviceService } from '../userservice.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,13 @@ import { NewsService } from '../news.service';
 })
 export class HomePage {
   news:any[] = []
+  now_login_username = ""
 
-
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService, private userService: UserserviceService) {}
 
   ngOnInit() {
     this.news = this.newsService.news
+    this.now_login_username = this.userService.now_username_login
   }
 
   addLike(i: number) {
